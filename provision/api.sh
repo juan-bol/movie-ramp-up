9937992c6a1ddc82b8ebc669fb3f2ccc7294292a
juan-bol <juan-bol98@hotmail.com>
2021-01-27 10:12:35 -0500

    secrets added to api provision

--- provision/api.sh
+++ provision/api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install software-properties-common curl -y
+sudo apt install software-properties-common curl net-tools -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
 sudo apt install git nodejs -y
 git clone https://github.com/juan-bol/movie-analyst-api
@@ -12,4 +12,5 @@ sudo npm install mariadb
 sudo apt install mariadb-client -y
 mysql -h 192.168.100.13 -P 3306 --user="root" --password="holi" movie_db < ./data_model/table_creation_and_inserts.sql
 
-node server.js &
+sudo bash -c 'echo "holi" >> .secrets'
+sudo node server.js &
