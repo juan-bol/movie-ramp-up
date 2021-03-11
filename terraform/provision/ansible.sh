6aeb70be8e5798e0a5c6dabbcb089be71941a163
juan-bol <juan-bol98@hotmail.com>
2021-03-11 00:47:55 -0500

    fixes in new structure of ansible

--- terraform/provision/ansible.sh
+++ terraform/provision/ansible.sh
@@ -4,5 +4,6 @@ sudo yum update -y
 sudo amazon-linux-extras install ansible2 -y
 sudo yum install git python2-pip python3-pip -y
 git clone https://github.com/juan-bol/movie-ramp-up
+chmod +x movie-ramp-up/ansible/inventory.py ec2.*
 pip install boto --user
 pip3 install boto3 --user
